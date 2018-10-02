3af1e8f9e117e17bfe0dacd251d620c2a7e76bb9
Daud Zoss <daud.zoss@dexcom.com>
2018-10-02 10:11:44 -0700

    more C testing, bextr seems not supported on this architecture (i5)

--- amd64/2048game.s
+++ amd64/2048game.s
@@ -2,7 +2,6 @@ nybmask	equ	0x0f0f0f0f0f0f0f0f
 	section	.data
 values:
 	db	"       ",0	;char values[] = {"       ",
-	db	"[   1] ",0	;                  "[   1] ",
 	db	"[   2] ",0	;                  "[   2] ",
 	db	"[   4] ",0	;                  "[   4] ",
 	db	"[   8] ",0	;                  "[   8] ",
@@ -19,7 +18,7 @@ values:
 	db	"[2^14] ",0	;                  "[2^14] ",
 	db	"[2^15] ",0	;                  "[2^15] "};
 newrow:	
-	db	10,0		;char newrow = '\n';
+	db	10,0		;char newrow[] = "\n";
 	
 	section	.text
 	global	print4x4
@@ -28,17 +27,17 @@ print4x4:
 	push	rbp		;void print4x4(uint64_t rdi) {
 	mov	rbp,rsp		;
 	lea	rsp,[rsp-32]	; auto
-	mov	[rsp+0],rbx	;
-	mov	[rsp+8],r12	;
-	
 	lea	rsi,[rsp+16]	;  uint8_t rsi[16];
-	mov	rcx,nybmask	;
+	mov	[rsp+8],r12	; rdi =(rdi&(0xff<<56)>>56)|(rdi&(0xff<<48)>>48)
+	mov	[rsp+0],rbx	;     |(rdi&(0xff<<40)>>40)|(rdi&(0xff<<32)>>32)
+	mov	rcx,nybmask	;     |(rdi&(0xff<<24)>>24)|(rdi&(0x16<<16)>>56)
+	bswap	rdi		;     |(rdi&(0xff<<8)>>8)  |(rdi&(0xff<<0)>>0);
 	mov	rax,rdi		; // put pre-located tiles into rows 0,1
 	and	rax,rcx		; for (int i = 0; i < 8; i++)
-	mov	[rsi+0],rax	;  rsi[i] = (rdi >> (i*8)) & 0x0f;
+	mov	[rsi+8],rax	;  rsi[i+8] = (rdi >> (i*8)) & 0x0f;
 	shr	rdi,4		; // put interleaved tiles into rows 2,3
 	and	rdi,rcx		; for (int i = 0; i < 8; i++)
-	mov	[rsi+8],rdi	;  rsi[i+8] = ((rdi>>4) >> (i*8)) & 0x0f;
+	mov	[rsi+0],rdi	;  rsi[i+0] = ((rdi>>4) >> (i*8)) & 0x0f;
 
 .L1print4x4:
 	mov	ebx,[rsi]	; for (int i = 0; i < 4; i++) {
@@ -68,18 +67,18 @@ print4x4:
 	pop	rbp		;
 	ret			;}
 
-%if 0
-%macro	bextr	3
-	mov	%1,%2
-	shr	%1,%3&0x3f
-	and	%1,(1<<(%3>>8))-1
-%endmacro
-%macro	cmovne	2
+%macro	_cmovne	2
 	jne	%%equal
 	mov	%1,%2
 %%equal:	
 %endmacro
-%endif
+	
+%macro	_bextr	3
+	cpuid
+	mov	%1,%2
+	shr	%1,%3&0x3f
+	and	%1,(1<<(%3>>8))-1
+%endmacro
 	
 	global	empties
 empties:
@@ -88,7 +87,7 @@ empties:
 	xor	rax,rax		; for (i = 0; i < 0x10; i++) {
 %assign f 0x400
 %rep 16
-	bextr	rsi,rdi,f	;  // determine whether the cell is clear (1)
+	_bextr	rsi,rdi,f	;  // determine whether the cell is clear (1)
 	setz	cl		;  uint8_t c = (rdi & (0xf << (4*i))) ? 0 : 1;
 	add	cl,al		;
 	xor	dl,dl		;  // cl == 1+al if clear, cl == al if not clear
